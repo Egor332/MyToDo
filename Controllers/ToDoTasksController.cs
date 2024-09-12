703364df16ed60bef19ec4eb9e68025689c04150
Yahor Kachanouski <kegor2323@gmail.com>
2024-09-12 16:39:15 +0200

    Index view started, create prototypes created

---
+++ Controllers/ToDoTasksController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyToDo.Data;
+using MyToDo.Models;
+
+namespace MyToDo.Controllers
+{
+    [Authorize]
+    public class ToDoTasksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoTasksController(ApplicationDbContext context) { _context = context; }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _context.Tasks.ToListAsync());
+        }
+
+        // GET request
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id, Name, Description, StatusCode, UserId")] ToDoTask task)
+        {
+            // temporary code
+            return View();
+        }
+    }
+}
