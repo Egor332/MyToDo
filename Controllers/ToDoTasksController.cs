e1598381cdc12ad9f8c1406a54b742273fe38cfc
Yahor Kachanouski <kegor2323@gmail.com>
2024-09-15 13:44:05 +0200

    Edit functionality is added

--- Controllers/ToDoTasksController.cs
+++ Controllers/ToDoTasksController.cs
@@ -16,7 +16,8 @@ namespace MyToDo.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var model = await _context.Tasks.Where(t => t.UserId == HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value).ToListAsync();
+            var model = await _context.Tasks.
+                Where(t => t.UserId == HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value).ToListAsync(); // changed from: 
             return View(model);
         }
 
@@ -38,6 +39,63 @@ namespace MyToDo.Controllers
                 return RedirectToAction("Index");
             }
             return View(task);
+        }
+
+        // GET request
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            ToDoTask task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound(); // may be change to task does not exist
+            }
+            if (task.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+                return NotFound(); // may be change to access deny
+            }
+            return View(task);
+        }
+
+        // POST request
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Description, StatusCode, UserId")] ToDoTask task)
+        {
+            if (id != task.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Tasks.Update(task);
+                    await _context.SaveChangesAsync();
+                } 
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (IsTaskExist(id))
+                    {
+                        throw;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        public bool IsTaskExist(int id)
+        {
+            return _context.Tasks.Any(task => task.Id == id);
         }
     }
 }
